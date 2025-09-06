58bb98cb4932472e1308d7bb80ed4c7da404231b
Jonathan S <js3294@cornell.edu>
2025-09-06 13:26:05 -0400

    Updated picture and bio

--- src/components/About/AboutStyles.jsx
+++ src/components/About/AboutStyles.jsx
@@ -1,24 +1,32 @@
 import styled from 'styled-components';
 
-
 export const AboutContainer = styled.div`
-display: grid;
-grid-template-columns: 35% 65%;
-padding-left: 12%;
-padding-right: 12%;
-gap: 5%;
-align-items: center;
-`
+  display: grid;
+  grid-template-columns: 40% 60%;
+  padding-left: 10%;
+  padding-right: 10%;
+  gap: 2rem;
+  align-items: center;
+
+  @media (min-width: 1920px){
+    padding-left: 15%;
+    padding-right: 15%;
+  }
+`;
+
+export const HeadshotWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const Headshot = styled.img`
-  max-width: 100%;
-  height: auto;
-  border-radius: 8px;
-  display: block;
-  background-color: green;
+  width: 300px;
+  height: 300px;
+  border-radius: 5%;
   object-fit: cover;
 `;
 
 export const Bio = styled.div`
-  font-size: clamp(10px, 2vw, 22px);
+  font-size: clamp(14px, 2vw, 20px);
 `;
